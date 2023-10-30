559df478c72d2440712e5575a3e92fd25ef75c10
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2023-10-31 01:51:16 +0530

    bug fixed

--- src/pages/our-brands.jsx
+++ src/pages/our-brands.jsx
@@ -259,9 +259,9 @@ function our_brands() {
   return (
     <>
       <Navbar></Navbar>
-      <div style={{ "backgroundImage": `url(${banner_image.src})` }} className='hero-banner' />
+      <div style={{ "backgroundImage": `url(${banner_image.src})` }} className='hero-banner our-brands-banner'  />
       <div className="bg-light pb-4">
-        <div className="section-title text-center pb-4 pt-4 mt-5" >
+        <div className="section-title text-center pb-4 pt-4 mt-5"  >
           <h2 className='our-brands'>Our brands</h2>
         </div>
         <div className="container pb-4">
